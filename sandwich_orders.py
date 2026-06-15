c1cfa75253adf9194981d20674a0370800848052
Komalpreet Kaur <randhawakomal243@gmail.com>
2026-06-14 20:37:51 -0400

    Display finished sandwiches list

--- sandwich_orders.py
+++ sandwich_orders.py
@@ -6,3 +6,7 @@ while sandwich_orders:
     sandwich = sandwich_orders.pop(0)
     print("I made your " + sandwich)
     finished_sandwiches.append(sandwich)
+
+print("\nFinished sandwiches:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
