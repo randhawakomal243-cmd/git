c0569a14dcfa7565e4d18e0f9494ae34e4a42470
Komalpreet Kaur <randhawakomal243@gmail.com>
2026-06-14 20:37:23 -0400

    Process sandwich orders with loop

--- sandwich_orders.py
+++ sandwich_orders.py
@@ -1,3 +1,8 @@
 sandwich_orders = ["Tuna", "BLT", "Grilled Cheese", "Club"]
 
 finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop(0)
+    print("I made your " + sandwich)
+    finished_sandwiches.append(sandwich)
